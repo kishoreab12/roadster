303732ee26b9dbca8ca2caa4732475d8ccaa43dc
kishore kumar <kishoreab12@gmail.com>
2026-09-02 17:23:32 +0530

    mod

--- src/main/java/com/roadster/RoadsterApplication.java
+++ src/main/java/com/roadster/RoadsterApplication.java
@@ -2,11 +2,16 @@ package com.roadster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class RoadsterApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RoadsterApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(RoadsterApplication.class, args);
 	}
